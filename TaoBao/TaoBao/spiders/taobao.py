40e9e48ad2a8e975fabbe32e850398820bd284d9
nola222 <1422450420@qq.com>
2018-03-06 20:04:37 +0800

    將搜索抓娃娃關鍵字返回的頁面寫入html文件

--- TaoBao/TaoBao/spiders/taobao.py
+++ TaoBao/TaoBao/spiders/taobao.py
@@ -3,10 +3,12 @@ import scrapy
 
 
 class TaobaoSpider(scrapy.Spider):
-    name = 'taobao'
-    allowed_domains = ['taobao.com']
-    # start_urls = ['https://s.taobao.com/search?q=zhuawawa&s=44']
-    start_urls = ['https://www.taobao.com']
+	name = 'taobao'
+	allowed_domains = ['taobao.com']
+	# start_urls = ['https://s.taobao.com/search?q=zhuawawa&s=44']
+	start_urls = ['https://www.taobao.com']
 
-    def parse(self, response):
-        print(response.body.decode('utf-8'))
+	def parse(self, response):
+		with open('zhuawawa_info.html','w+',encoding='utf-8') as f:
+			f.write(response.body.decode('utf-8'))
+		# print(response.body.decode('utf-8'))
